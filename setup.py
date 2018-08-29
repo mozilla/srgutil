a3e84a390e1efdb155767ce204fbb03d32d8a61a
Sarah Bird <birdsarah@mozilla.com>
2018-08-29 15:19:31 -0500

    Bump test lib versions, add python 3.7
    There was a bug for py37 in earlier moto

--- setup.py
+++ setup.py
@@ -45,8 +45,11 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Scientific/Engineering :: Information Analysis'
     ],
